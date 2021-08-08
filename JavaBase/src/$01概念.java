fb7cf0f392b79fc2bb17535f0b4ee50dea50e3b4
root <xpblog@163.com>
2021-08-08 16:46:12 +0800

    feat:概念篇

---
+++ JavaBase/src/$01概念.java
@@ -0,0 +1,46 @@
+public class $01概念 { // **切记此处的Java类名，命名十分不规范，但是为了方便阅读所以才这样命名**
+
+    /**
+     * 什么是Java?(ps:此处的J是大写，ava是小写，请保持良好的习惯，不要写JAVA、java、jAVA...)
+     *
+     * Java是一门面向对象的编程语言
+     * 那么此处引出两个词，"编程语言"、"面向对象"
+     *  - 编程语言，就是编写程序的语言。
+     *  - 面向对象，是面向过程而言的，是一种思想
+     *    此处可以简单理解：面向过程编写程序，代码只能用一次，一次性的。但面向对象代码是可以"复用"了
+     */
+
+
+    /**
+     * Java之父詹姆斯·高斯林（James Gosling）
+     *
+     * 1990年，詹姆斯·高斯林（James Gosling）与Patrick Naughton和Mike Sheridan等人合作“绿色计划”，
+     * 后来发展一套语言叫做“Oak”，后改名为Java。1994年底，James Gosling在硅谷召开的“技术、教育和设计大
+     * 会”上展示Java程式。2000年，Java成为世界上最流行的电脑语言。 --百度百科
+     */
+
+
+    /**
+     * Java文件格式
+     *
+     * Java编写的代码源文件格式是.java
+     *  - 说白了，就是程序员写完代码保存的文件格式，和你保存文档.txt,.doc等等类似
+     * Java运行的编译后的文件格式是.class
+     *  - 说白了，就是程序员编写的代码发挥作用、运行时候的文件格式是.class
+     */
+
+
+    /**
+     * Java三件套，jvm、jre、jdk
+     *
+     * jvm：Java Virtual Machinea（Java虚拟机）
+     *  - Java虚拟机是运行.class文件的地方
+     *
+     * jre：Java Runtime Environment（Java运行环境）
+     *  - jre包含了jvm和一些核心类库，一些运行Java程序的机器装上jre就可以跑Java程序了
+     *
+     * jdk：Java Development Kit（Java开发员的软件开发工具包）
+     *  - jdk包含了jre和Java工具（编译工具、打包工具等等），Java程序员编写Java程序时需要安装
+     */
+
+}
