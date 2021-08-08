509ec7f7f0857703fc3751954ea36ee8c28936f5
root <xpblog@163.com>
2021-08-08 17:27:27 +0800

    feat:打出第一行代码

---
+++ 01JavaBase/src/$02HelloWorld.java
@@ -0,0 +1,16 @@
+public class $02HelloWorld {
+
+    /**
+     * Java的运行入口是一个main方法，以后不管你的代码有多复杂，多牛逼
+     * 最后都会显性或者隐形的通过进入main方法执行。
+     *
+     * 让我们开始敲击自己的第一行代码吧，HelloWorld
+     * 如果你的编辑器是Idea，打出HelloWorld并不费劲
+     * 快捷键：psvm生成main方法
+     *       sout打出输出语句
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
